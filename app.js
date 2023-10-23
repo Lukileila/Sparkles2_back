18ced97c485a7ae7fb665942de89a34e41da7060
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-10-23 17:31:34 +0200

    backend str

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app
+.get('/', (req,res) => {
+    res.send('x')
+});
+.post()
+.put()
+.delete();
+
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log('Example app listening on ${port}'))
