d5e185b2e31d0a0eb147611bc7e5d6ecc7026425
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-11-02 14:44:12 +0100

    comments api

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const {getAllProducts, getSingleProduct} = require('./controllers/productControllers');
+const {getCommentFromSingleProduct} = require('./controllers/commentController')
+
 
 const cors = require('cors');
 const port = process.env.PORT || 3000;
@@ -13,5 +15,7 @@ app.get('/', (req, res) => res.send('Welcome to the Sparkles API!'));
 app.route('/products').get(getAllProducts);
 
 app.route('/products/:productId').get(getSingleProduct);
+
+app.route('/products/:productId/comments').get(getCommentFromSingleProduct);
 
 app.listen(port, () => console.log(`Example app listening on ${port}`)); 
